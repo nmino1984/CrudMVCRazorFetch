77db0c5d17f8e0355d41404fb8f5ced8992fbc15
Noel Mino Herrera <o.kindelan@gmail.com>
2022-12-15 02:13:51 -0500

    All changes in initial video mades

---
+++ Controllers/PeopleController.cs
@@ -0,0 +1,37 @@
+﻿using CrudMVCRazorFetch.Models;
+using CrudMVCRazorFetch.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrudMVCRazorFetch.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            List<PeopleViewModel> listPeopleViewModels = new List<PeopleViewModel>();
+            
+            using (CrudMVCRazorFetchEntities db = new CrudMVCRazorFetchEntities())
+            {
+                listPeopleViewModels = (from d in db.people
+                                       orderby d.age descending
+                                       select new PeopleViewModel { 
+                                            Id= d.id,
+                                            Name= d.name,
+                                            Age= d.age
+                                       }).ToList<PeopleViewModel>();
+
+            }
+            return View(listPeopleViewModels);
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CrudMVCRazorFetch.MvcApplication" Language="C#" %>
